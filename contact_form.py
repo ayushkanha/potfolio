be4f7052f056b4036005300d987a47122a8dc8a9
ayushkanha <61038773+ayushkanha@users.noreply.github.com>
2025-03-18 17:32:04 +0530

    Update contact_form.py

--- contact_form.py
+++ contact_form.py
@@ -6,42 +6,35 @@ import json
 def contact():
     col1, col2 = st.columns(2)
 
-    # col1.markdown("## Experience")
     with col1:
         st.markdown("""
-                    <style>
-                    .centered {
-                        display: flex;
-                        align-items: center;
-                        height: 100%;
-                        margin-top: 50px;
-                    }
-                    </style>
-                    <div class="centered">
-                        <h2>📨 Contact Info. </h2>
-                    </div>
-                """, unsafe_allow_html=True)
+            <style>
+            .centered {
+                display: flex;
+                align-items: center;
+                height: 100%;
+                margin-top: 50px;
+            }
+            </style>
+            <div class="centered">
+                <h2>📨 Contact Info. </h2>
+            </div>
+        """, unsafe_allow_html=True)
+
     path = "contact.json"
     with open(path, "r") as file:
         url = json.load(file)
+
     with col2:
-        st_lottie(url,
-                  reverse=True,
-                  height=200,
-                  width=200,
-                  speed=1,
-                  loop=True,
-                  quality='high',
-                  )
-        
-    st.markdown("<h2 style='text-align: left; '>Phone : +91-9479280486</h2>", unsafe_allow_html=True)
+        st_lottie(url, reverse=True, height=200, width=200, speed=1, loop=True, quality='high')
+
+    st.markdown("<h2 style='text-align: left;'>📞 Phone : +91-9479280486</h2>", unsafe_allow_html=True)
     
-    url = "ayushkanh19@gmail.com"
-    st.header("******Mail : [ayushkanha19@gmail.com](%s)******" % url)
+    st.markdown("📧 **Mail** : [ayushkanha19@gmail.com](mailto:ayushkanha19@gmail.com)")
     
-    url = "https://linkedin.com/in/ayush-kumar-sahu-299b8b23b"
-    st.header("******Linkdin : [Link](%s)******" % url)
-    url = "https://github.com/ayushkanha"
-    st.header("******Github : [Link](%s)******" % url)
+    st.markdown("🔗 **LinkedIn** : [LinkedIn Profile](https://linkedin.com/in/ayush-kumar-sahu-299b8b23b)")
+    
+    st.markdown("💻 **GitHub** : [GitHub Profile](https://github.com/ayushkanha)")
+
 
 
